b3272fb0fff44cda924f8d0d603e75e8f91f625a
acumic11622rn <acumic11622rn@raf.rs>
2023-11-06 12:56:17 +0100

    proveriti pom izmeniti datum StudentImportExportOpenCSV.java

---
+++ raspored_stefan_aleksandar_specifikacija/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Termin termin;
+
+        RasporedPisiCitaj rasporedPisiCitaj = new RasporedPisiCitaj("raspored_stefan_aleksandar_specifikacija/src/main/java/resursi/termini.csv");
+//        rasporedPisiCitaj.ucitajPodatke();
+
+    }
+}
